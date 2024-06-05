3240ec34ef004121584ae5caf679db9182e5c802
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-05 22:08:55 +0800

    updated code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,20 +8,6 @@ return {
 		},
 	},
 
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
-
 	"RRethy/vim-illuminate",
 	"ThePrimeagen/vim-be-good",
 	"eandrju/cellular-automaton.nvim",
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,26 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local oil = require("oil")
-		oil.setup()
-		vim.keymap.set("n", "<leader>pv", oil.toggle_float, {})
-	end,
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local oil = require("oil")
+			oil.setup()
+			vim.keymap.set("n", "<leader>pv", oil.toggle_float, {})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
