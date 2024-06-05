420cc9c923481146beb8441db27ce14823102cf5
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-05 18:08:21 +0800

    updated code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,14 +23,14 @@ return {
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
+				callback = function()
 					vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, {})
 					vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, {})
 					vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
 					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 					vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.declaration, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.declaration, {})
 				end,
 			})
 		end,
