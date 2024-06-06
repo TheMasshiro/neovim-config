a946069a2187f3e11f70475c5ce691f0a11ad703
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-06 12:03:56 +0800

    changed python3 path

--- init.lua
+++ init.lua
@@ -23,4 +23,4 @@ require("core.remap")
 require("core.set")
 require("core.yank")
 
-vim.g.python3_host_prog = "C:\\Users\\Masshiro\\scoop\\shims\\python3.exe"
+vim.g.python3_host_prog = vim.fn.expand("~/scoop/shims/python3.exe")
