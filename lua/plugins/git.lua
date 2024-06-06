8cdb20b47699b542b4350ccd575b23bb6a9ec4d3
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-06 11:43:35 +0800

    added todo-comment plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,8 +7,22 @@ return {
 		config = function()
 			require("gitsigns").setup()
 
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Toggle Preview Hunk" })
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Git Blame" })
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup({
+				signs = false,
+				highlight = {
+					keyword = "",
+					after = "",
+				},
+			})
+			vim.keymap.set("n", "<leader>pt", ":TodoTelescope<CR>", { desc = "Open Todo-Comments" })
 		end,
 	},
 }
