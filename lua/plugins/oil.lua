138207dbd42bbe9ca8c6d32878f37d464ffcffb0
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-06 11:42:44 +0800

    moved which-key plugin

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
 		config = function()
 			local oil = require("oil")
 			oil.setup()
-			vim.keymap.set("n", "<leader>pv", oil.toggle_float, {})
+			vim.keymap.set("n", "<leader>pv", oil.toggle_float, { desc = "Open Parent Directory" })
 		end,
 	},
 	{
