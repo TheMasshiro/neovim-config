86a1b23aef49ab01de9502849379cfaf1c78cc74
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-08 18:22:11 +0800

    Updated Code

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -20,4 +20,14 @@ return {
 			vim.keymap.set("n", "<leader>pt", ":TodoTelescope<CR>", { desc = "Open Todo-Comments" })
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
