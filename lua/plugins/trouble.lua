b6259d3aee7e19d5625bd7093842f4e1f1bbda9f
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-08 15:51:35 +0800

    Fixed trouble.nvim

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,18 +1,34 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>xl",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xq",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
 	},
-
-	config = function()
-		vim.keymap.set(
-			"n",
-			"<leader>xq",
-			"<cmd>TroubleToggle quickfix<cr>",
-			{ silent = true, noremap = true, desc = "Trouble Quickfix" }
-		)
-	end,
 }
