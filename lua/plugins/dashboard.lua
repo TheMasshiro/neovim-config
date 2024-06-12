c120098b7d81f87b74dbb6d2796ff70f6518f782
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-12 14:34:10 +0800

    Added dashboard nvim as greeter

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,33 @@
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Find Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						desc = " File Manager",
+						group = "Number",
+						action = "Oil --float",
+						key = "v",
+					},
+				},
+				mru = { limit = 5 },
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
