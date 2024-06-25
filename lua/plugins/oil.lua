b15522c22351fd61dded2bef831896eb50af3019
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-25 09:54:03 +0800

    Removed ESC keymapping

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,11 +4,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local oil = require("oil")
-		oil.setup({
-			keymaps = {
-				["<Esc>"] = { callback = "actions.close", mode = "n" },
-			},
-		})
+		oil.setup({})
 		vim.keymap.set("n", "<leader>pv", oil.toggle_float, { desc = "Open Parent Directory" })
 	end,
 }
