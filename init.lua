71ddce9bd01cf524b08edbf679b5efd57fa6a452
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-25 10:15:54 +0800

    Moved vim.opts(core.set) to lazy.lua
    as per documentation of lazy I moved vim.opts to lazy lua file

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require("config.lazy")
 require("core.remap")
-require("core.set")
 require("core.yank")
 
 vim.g.python3_host_prog = vim.fn.expand("~/scoop/shims/python3.exe")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,9 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- vim.opts
+require("core.set")
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
