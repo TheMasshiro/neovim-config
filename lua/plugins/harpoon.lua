c41877e891e741ae796faf2e4d32847fa3f2ba88
John Christian Vicente <xtianfurlow1223@gmail.com>
2024-06-28 17:11:41 +0800

    Changed harpoon to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,23 +1,29 @@
 return {
-	"theprimeagen/harpoon",
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>ma", mark.add_file, { desc = "Add Buffer to Harpoon" })
-		vim.keymap.set("n", "<leader>mq", ui.toggle_quick_menu, { desc = "Harpoon Quick Menu" })
+		vim.keymap.set("n", "<leader>qq", function()
+			harpoon:list():add()
+		end, { desc = "Add Buffer to Harpoon" })
+		vim.keymap.set("n", "<leader>qw", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon Quick Menu" })
 
-		vim.keymap.set("n", "<leader>m1", function()
-			ui.nav_file(1)
+		vim.keymap.set("n", "<leader>q1", function()
+			harpoon:list():select(1)
 		end, { desc = "Harpoon Buffer 1" })
-		vim.keymap.set("n", "<leader>m2", function()
-			ui.nav_file(2)
+		vim.keymap.set("n", "<leader>q2", function()
+			harpoon:list():select(2)
 		end, { desc = "Harpoon Buffer 2" })
-		vim.keymap.set("n", "<leader>m3", function()
-			ui.nav_file(3)
+		vim.keymap.set("n", "<leader>q3", function()
+			harpoon:list():select(3)
 		end, { desc = "Harpoon Buffer 3" })
-		vim.keymap.set("n", "<leader>m4", function()
-			ui.nav_file(4)
+		vim.keymap.set("n", "<leader>q4", function()
+			harpoon:list():select(4)
 		end, { desc = "Harpoon Buffer 4" })
 	end,
 }
